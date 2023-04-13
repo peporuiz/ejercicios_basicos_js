d8b978066eb5165afda022c8756cfb050dd7cc18
peporuiz <112805291+peporuiz@users.noreply.github.com>
2023-04-13 19:35:22 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+//Array 12 meses año
+
+const stringArray = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'
+, 'Junio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+
+for(let i = 0; i<stringArray.length;i++){
+    console.log(stringArray[i])
+}
+
+//console.log(stringArray)
